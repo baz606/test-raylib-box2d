b4c7222fa7c1c2d2bf73d248d2a6c50161f9fe0a
baz606 <baz6066@gmail.com>
2024-01-20 16:03:40 -0600

    create hello box2d test from box2d.org/documentation

--- main.cpp
+++ main.cpp
@@ -1,12 +1,47 @@
-#include <iostream>
 #include "raylib.h"
 #include <box2d/box2d.h>
+#include <cstdio>
 
 int main()
 {
-  std::cout << "Hello, World!" << std::endl;
   b2Vec2 gravity(0.0f, -10.0f);
-  std::cout << gravity.Length() << std::endl;
+  b2World world(gravity);
+
+  // Ground body
+  b2BodyDef groundBodyDef;
+  groundBodyDef.position.Set(0.0f, -10.0f);
+  b2Body* groundBody = world.CreateBody(&groundBodyDef);
+
+  b2PolygonShape groundBox;
+  groundBox.SetAsBox(50.0f, 10.0f);
+
+  groundBody->CreateFixture(&groundBox, 0.0f);
+
+  // Dynamic body
+  b2BodyDef bodyDef;
+  bodyDef.type = b2_dynamicBody;
+  bodyDef.position.Set(0.0f, 4.0f);
+  b2Body* body = world.CreateBody(&bodyDef);
+
+  b2PolygonShape dynamicBox;
+  dynamicBox.SetAsBox(1.0f, 1.0f);
+  b2FixtureDef fixtureDef;
+  fixtureDef.shape = &dynamicBox;
+  fixtureDef.density = 1.0f;
+  fixtureDef.friction = 0.3f;
+  body->CreateFixture(&fixtureDef);
+
+  float timeStep = 1.0f / 60.0f;
+  int32 velocityIterations = 6;
+  int32 positionIterations = 2;
+
+  for(int32 i = 0; i < 60; ++i)
+  {
+    world.Step(timeStep, velocityIterations, positionIterations);
+    b2Vec2 position = body->GetPosition();
+    float angle = body->GetAngle();
+    printf("%0.2f %0.2f %0.2f\n", position.x, position.y, angle);
+  }
 
   return 0;
 }
