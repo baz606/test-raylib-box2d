73ada23c9257239efad47cb5c153642f09cd658a
baz606 <baz6066@gmail.com>
2024-01-20 20:54:58 -0600

    After some reading on scaling physics units to pixels, I was able to successfully add some rendering to the Hello Box2D tutorial

--- main.cpp
+++ main.cpp
@@ -1,47 +1,98 @@
 #include "raylib.h"
 #include <box2d/box2d.h>
-#include <cstdio>
+
+// Pixels per meter
+const float PPM = 100.f;
 
 int main()
 {
-  b2Vec2 gravity(0.0f, -10.0f);
+  // Init raylib
+  int screenWidth = 1024;
+  int screenHeight = 640;
+  float scale = 100.f;
+  InitWindow(screenWidth, screenHeight, "test-ray-physics");
+  SetTargetFPS(60);
+
+  Rectangle rGroundBody, rDynamicBody;
+
+  // Init physics
+  b2Vec2 gravity(0.0f, 10.0f);
   b2World world(gravity);
 
   // Ground body
   b2BodyDef groundBodyDef;
-  groundBodyDef.position.Set(0.0f, -10.0f);
+  groundBodyDef.position.Set((screenWidth / 2.f) / PPM, ((screenHeight / 2.f) + 200.f) / PPM);
+  rGroundBody.x = groundBodyDef.position.x * PPM;
+  rGroundBody.y = groundBodyDef.position.y * PPM;
   b2Body* groundBody = world.CreateBody(&groundBodyDef);
 
+  float groundBoxWidth = 500.f;
+  float groundBoxHeight = 100.f;
   b2PolygonShape groundBox;
-  groundBox.SetAsBox(50.0f, 10.0f);
+  groundBox.SetAsBox((groundBoxWidth / 2.f) / PPM, (groundBoxHeight / 2.f) / PPM);
+  rGroundBody.width = groundBoxWidth;
+  rGroundBody.height = groundBoxHeight;
 
   groundBody->CreateFixture(&groundBox, 0.0f);
 
   // Dynamic body
   b2BodyDef bodyDef;
   bodyDef.type = b2_dynamicBody;
-  bodyDef.position.Set(0.0f, 4.0f);
+  float dropHeight = screenHeight / 4.f;
+  bodyDef.position.Set((screenWidth / 2.f) / PPM, ((screenHeight / 2.f) - dropHeight) / PPM);
+  rDynamicBody.x = bodyDef.position.x * PPM;
+  rDynamicBody.y = bodyDef.position.y * PPM;
   b2Body* body = world.CreateBody(&bodyDef);
 
+  float dynamicBodyWidth = 50.0f;
+  float dynamicBodyHeight = 50.0f;
   b2PolygonShape dynamicBox;
-  dynamicBox.SetAsBox(1.0f, 1.0f);
+  dynamicBox.SetAsBox((dynamicBodyWidth / 2.0f) / PPM, (dynamicBodyHeight / 2.0f) / PPM);
+  rDynamicBody.width = dynamicBodyWidth;
+  rDynamicBody.height = dynamicBodyHeight;
   b2FixtureDef fixtureDef;
   fixtureDef.shape = &dynamicBox;
   fixtureDef.density = 1.0f;
   fixtureDef.friction = 0.3f;
-  body->CreateFixture(&fixtureDef);
+  b2Fixture* fixture = body->CreateFixture(&fixtureDef);
 
   float timeStep = 1.0f / 60.0f;
   int32 velocityIterations = 6;
   int32 positionIterations = 2;
 
-  for(int32 i = 0; i < 60; ++i)
+  Vector2 origin = { 0 };
+  float physicsToWorldScale = 100.f;
+  while (!WindowShouldClose())
   {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+//    TraceLog(LOG_ERROR, "Mouse(%d, %d)", GetMouseX(), GetMouseY());
+
+     // Process input
+    if (IsKeyReleased(KEY_R))
+    {
+      body->DestroyFixture(fixture);
+      world.DestroyBody(body);
+
+      body = world.CreateBody(&bodyDef);
+      fixture = body->CreateFixture(&fixtureDef);
+    }
+
+    // Physics update
     world.Step(timeStep, velocityIterations, positionIterations);
     b2Vec2 position = body->GetPosition();
-    float angle = body->GetAngle();
-    printf("%0.2f %0.2f %0.2f\n", position.x, position.y, angle);
+    rDynamicBody.x = position.x * PPM;
+    rDynamicBody.y = position.y * PPM;
+
+    // Render update
+    origin = { rGroundBody.width / 2.f, rGroundBody.height / 2.f };
+    DrawRectanglePro(rGroundBody, origin, 0.f, BLUE);
+    origin = { rDynamicBody.width / 2.f, rDynamicBody.height / 2.f };
+    DrawRectanglePro(rDynamicBody, origin, 0.f, BLACK);
+
+    EndDrawing();
   }
+  CloseWindow();
 
   return 0;
 }
